12ec6cc07b2a8de5f086df32e7b750aebc8ff205
sajin <sajin@bridge.com>
2015-07-09 18:31:09 +0530

    desgin changes

--- www/bucketlist/controller.bucketlist.js
+++ www/bucketlist/controller.bucketlist.js
@@ -1,4 +1,16 @@
 angular.module('controller.bucketlist',[])
-	.controller('bucketlistCtrl', ['$scope', function($scope){
-		console.log('bucketlist:',$scope);		
+	.controller('bucketlistCtrl', ['$scope','$http', function($scope,$http){
+		console.log('bucketlist:',$scope);
+		$scope.book={};
+		$scope.search = function(){
+			console.log('find:',$scope.book.query);
+			$http.get('https://www.googleapis.com/books/v1/volumes?q=pride+prejudice').
+				success(function(data){
+					console.log('data:',data.items);
+					$scope.results=data.items;
+				}).
+				error(function(error){
+					console.log('error:',error);
+				})
+		}		
 	}])
